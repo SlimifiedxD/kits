76f5ba08c8f014912b176f05158e8f60b2128d4d
Aaryn Tiger Malik <godofthemonki@gmail.com>
2026-03-24 17:07:01 -0400

    Fix config-related inconsistencies

--- src/main/kotlin/org/slimecraft/kits/data/config/Config.kt
+++ src/main/kotlin/org/slimecraft/kits/data/config/Config.kt
@@ -1,7 +1,6 @@
 package org.slimecraft.kits.data.config
 
 import com.sksamuel.hoplite.decoder.Minutes
-import net.kyori.adventure.text.Component
 import org.slimecraft.kits.data.config.dto.KitSettings
 
-data class Config(val mapResetFloor: String, val dynamiteExplodeTicks: Int, val mapResetCooldown: Minutes, val kitSettings: KitSettings, val coinsEarnedMessage: String, val coinsPerCoinBlockMined: Double, val coinsPerPlayerKilled: Double, val coinBlock: String, val notEnoughCoinsForWithdrawal: String, val withdrawalSuccess: String, val withdrawalUsed: String, val cannotWithdrawNegativeAmount: String)
+data class Config(val mapResetFloor: String, val dynamiteExplodeTicks: Int, val mapResetCooldown: Minutes, val kitSettings: KitSettings, val coinsEarnedMessage: String, val coinsPerCoinBlockMined: Double, val coinsPerPlayerKilled: Double, val coinBlock: String, val notEnoughCoinsForWithdrawal: String, val withdrawalSuccess: String, val withdrawalUsed: String, val cannotWithdrawInvalidAmount: String)
--- src/main/kotlin/org/slimecraft/kits/data/config/dto/PotionEffectDto.kt
+++ src/main/kotlin/org/slimecraft/kits/data/config/dto/PotionEffectDto.kt
@@ -5,7 +5,7 @@ import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
 import org.slimecraft.bedrock.kt.extensions.seconds
 
-data class PotionEffectDto(val id: String, val amplifier: Byte, val duration: Int, val ambient: Boolean, val showParticles: Boolean = true, val showIcon: Boolean = true) {
+data class PotionEffectDto(val id: String, val amplifier: Byte, val duration: Int, val ambient: Boolean = false, val showParticles: Boolean = true, val showIcon: Boolean = true) {
     fun potionEffect(): PotionEffect {
         return PotionEffect(Registry.POTION_EFFECT_TYPE.stream().filter { it.key.value().equals(id) }.toList()[0], duration.seconds.toInt(), amplifier.toInt(), ambient, showParticles, showIcon)
     }
