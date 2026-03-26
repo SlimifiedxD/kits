67dfa000b0c64fdf5e39aac1ecda4fff18433d01
Aaryn Tiger Malik <godofthemonki@gmail.com>
2026-03-26 10:15:44 -0400

    Add ability to take out multiple vouchers when withdrawing

--- src/main/kotlin/org/slimecraft/kits/command/WithdrawCommand.kt
+++ src/main/kotlin/org/slimecraft/kits/command/WithdrawCommand.kt
@@ -9,6 +9,8 @@ import org.bukkit.entity.Player
 import org.bukkit.persistence.PersistentDataType
 import org.slimecraft.bedrock.kt.extensions.component
 import org.slimecraft.bedrock.util.item.ItemBuilder
+import org.slimecraft.funmands.api.argument.type.DoubleArgument
+import org.slimecraft.funmands.api.argument.type.IntegerArgument
 import org.slimecraft.funmands.paper.command.PaperCommand
 import org.slimecraft.kits.data.config.Config
 import org.slimecraft.kits.withdrawalKey
@@ -16,26 +18,42 @@ import org.slimecraft.kits.withdrawalKey
 class WithdrawCommand(val economy: Economy, val reloadable: ReloadableConfig<Config>) : PaperCommand("withdraw") {
     init {
         addFormat("amount:double", {
-            val p = it.source.sender
-            if (p !is Player) return@addFormat
-            val amt: Double = it.get("amount")
-            val amtC = Component.text(amt)
-            if (amt <= 0) {
-                p.sendMessage(reloadable.getLatest().cannotWithdrawInvalidAmount.component("amount" to amtC))
-            }
-            if (!economy.has(p, amt)) {
-                p.sendMessage(reloadable.getLatest().notEnoughCoinsForWithdrawal.component("amount" to Component.text(amt), "balance" to Component.text(economy.getBalance(p))))
-                return@addFormat
-            }
-            val item = ItemBuilder.create()
-                .material(Material.PAPER)
-                .name("<aqua>Coin Withdrawal")
-                .lore(listOf("<white>A voucher for <yellow><amount></yellow> coins".component("amount" to Component.text(amt)).decorationIfAbsent(TextDecoration.ITALIC, TextDecoration.State.FALSE)))
-                .pdc(withdrawalKey, PersistentDataType.DOUBLE, amt)
-                .build()
-            economy.withdrawPlayer(p, amt)
-            p.sendMessage(reloadable.getLatest().withdrawalSuccess.component("amount" to amtC, "balance" to Component.text(economy.getBalance(p))))
-            p.inventory.addItem(item)
+            val p = it.source.sender as? Player ?: return@addFormat
+            withdraw(p, it.get("amount"))
         })
+
+        addFormat("amount:double vouchers:int", {
+            val p = it.source.sender as? Player ?: return@addFormat
+            withdraw(p, it.get("amount"), it.get("vouchers"))
+        })
+    }
+
+    private fun withdraw(p: Player, amtPerWithdrawal: Double, howManyVouchers: Int = 1) {
+        val amt = amtPerWithdrawal * howManyVouchers
+        val amtC = Component.text(amt)
+        if (amt <= 0) {
+            p.sendMessage(reloadable.getLatest().cannotWithdrawInvalidAmount.component("amount" to amtC))
+            return
+        }
+        if (howManyVouchers <= 0) {
+            // TODO: add config option saying invalid voucher amount
+            return
+        }
+        if (!economy.has(p, amt)) {
+            p.sendMessage(reloadable.getLatest().notEnoughCoinsForWithdrawal.component("amount" to Component.text(amt), "balance" to Component.text(economy.getBalance(p))))
+            return
+        }
+        val item = ItemBuilder.create()
+            .material(Material.PAPER)
+            .name("<aqua>Coin Withdrawal")
+            .lore(listOf("<white>A voucher for <yellow><amount></yellow> coins".component("amount" to Component.text(amt)).decorationIfAbsent(TextDecoration.ITALIC, TextDecoration.State.FALSE)))
+            .pdc(withdrawalKey, PersistentDataType.DOUBLE, amt)
+            .amount(howManyVouchers)
+            .build()
+        economy.withdrawPlayer(p, amt)
+        p.sendMessage(reloadable.getLatest().withdrawalSuccess.component("amount" to amtC, "balance" to Component.text(economy.getBalance(p))))
+        val items = p.inventory.addItem(item)
+
+        items.values.forEach { p.world.dropItem(p.location, it) }
     }
 }
