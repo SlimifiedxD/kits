e77e5813bfbc20e74ea2898c27e10b6cca1cac67
Aaryn Tiger Malik <godofthemonki@gmail.com>
2026-03-28 01:02:35 -0400

    Use deserializers

--- src/main/kotlin/org/slimecraft/kits/data/config/dto/ItemDto.kt
+++ src/main/kotlin/org/slimecraft/kits/data/config/dto/ItemDto.kt
@@ -3,9 +3,12 @@ package org.slimecraft.kits.data.config.dto
 import io.papermc.paper.datacomponent.DataComponentTypes
 import io.papermc.paper.datacomponent.item.ItemEnchantments
 import io.papermc.paper.datacomponent.item.PotionContents
+import io.papermc.paper.registry.RegistryAccess
+import io.papermc.paper.registry.RegistryKey
 import org.bukkit.Bukkit
 import org.bukkit.Color
 import org.bukkit.Material
+import org.bukkit.NamespacedKey
 import org.bukkit.inventory.ItemStack
 import org.bukkit.persistence.PersistentDataType
 import org.slimecraft.bedrock.kt.extensions.component
@@ -21,19 +24,18 @@ data class ItemDto(
     val name: String = "",
     val amount: List<Int> = listOf(1),
     val pdc: String = "",
-    val enchantments: Map<String, List<Int>> = emptyMap(),
+    val enchantments: List<EnchantmentDto> = emptyList(),
     val items: List<ItemDto> = listOf(),
     val potionContents: PotionContentsDto? = null,
-    val offhandEffects: List<PotionEffectDto> = listOf()
-) { // TODO: later make it a List<EnchantmentDto> and make it work with a custom deserializer
+    val offhandEffects: List<PotionEffectDto> = emptyList()
+) {
     fun itemStack(): ItemStack {
         val b = ItemBuilder.create()
             .material(Material.valueOf(material.uppercase()))
             .amount(amount[Random.nextInt(0, amount.size)])
             .component(
                 DataComponentTypes.ENCHANTMENTS,
-                ItemEnchantments.itemEnchantments(enchantments.map { EnchantmentDto(it.key, it.value) }
-                    .associate { it.enchantment() to it.levels[Random.nextInt(0, it.levels.size)] })
+                ItemEnchantments.itemEnchantments(enchantments.associate { RegistryAccess.registryAccess().getRegistry(RegistryKey.ENCHANTMENT).getOrThrow(NamespacedKey.minecraft(it.name)) to it.levels[Random.nextInt(0, it.levels.size)] })
             )
 
         if (!name.isEmpty()) {
